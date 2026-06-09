42ff62b5404fa9317833c76d95ee31ac180a3bf9
Pranshu1505 <pranshubarhat@email.com>
2026-06-09 11:20:14 +0530

    Update API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({ baseURL: 'https://rad-torrone-225b15.netlify.app/login/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
