ab1b31181f43a1891bead714f2e9a5f41c25b80b
Pranshu1505 <pranshubarhat@email.com>
2026-06-09 11:33:47 +0530

    Update backend API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'https://rad-torrone-225b15.netlify.app/login/api' });
+const api = axios.create({ baseURL: ' https://split-bill-app-yloh.onrender.com/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
